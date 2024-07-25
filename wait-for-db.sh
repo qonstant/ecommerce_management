7f2b5d5249143b47a94cf3a3949228bcd282c213
Rakymzhan Zhabagin <rakorako2204@gmail.com>
2024-07-25 16:51:26 +0500

    wait-for fix

--- wait-for-db.sh
+++ wait-for-db.sh
@@ -5,37 +5,8 @@ host="$1"
 shift
 cmd="$@"
 
-# Timeout in seconds
-timeout=30
+# Wait for 20 seconds
+sleep 20
 
-# Start time
-start_time=$(date +%s)
-
-# Run the command in the background and get its PID
-(
-  while ! pg_isready -h "$host" -p "5432"; do
-    >&2 echo "Postgres is unavailable - sleeping"
-    sleep 1
-  done
-  >&2 echo "Postgres is up - executing command"
-) &
-
-# PID of the background process
-bg_pid=$!
-
-# Wait for either the timeout or the background process to complete
-while kill -0 "$bg_pid" 2> /dev/null; do
-  current_time=$(date +%s)
-  elapsed_time=$((current_time - start_time))
-  
-  if [ $elapsed_time -ge $timeout ]; then
-    >&2 echo "Timeout after $timeout seconds - exiting"
-    kill -9 "$bg_pid" 2> /dev/null
-    exit 1
-  fi
-  
-  sleep 1
-done
-
-# Execute the command if the background process completed successfully
+# Execute the command
 exec $cmd
