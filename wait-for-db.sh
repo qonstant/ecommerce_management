b5a3acdd8df0fd23c48937e485438e9fc15427f7
Rakymzhan Zhabagin <rakorako2204@gmail.com>
2024-07-25 16:24:25 +0500

    deploy config

--- wait-for-db.sh
+++ wait-for-db.sh
@@ -5,6 +5,9 @@ host="$1"
 shift
 cmd="$@"
 
+# Wait for 1 minute (60 seconds) before starting to check if PostgreSQL is ready
+sleep 30
+
 until pg_isready -h "$host" -p "5432"; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
