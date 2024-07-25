90f605f8d6850d6abb91ab467855b40433137799
Rakymzhan Zhabagin <rakorako2204@gmail.com>
2024-07-25 16:36:48 +0500

    deploy config

--- wait-for-db.sh
+++ wait-for-db.sh
@@ -11,18 +11,31 @@ timeout=30
 # Start time
 start_time=$(date +%s)
 
-while ! pg_isready -h "$host" -p "5432"; do
+# Run the command in the background and get its PID
+(
+  while ! pg_isready -h "$host" -p "5432"; do
+    >&2 echo "Postgres is unavailable - sleeping"
+    sleep 1
+  done
+  >&2 echo "Postgres is up - executing command"
+) &
+
+# PID of the background process
+bg_pid=$!
+
+# Wait for either the timeout or the background process to complete
+while kill -0 "$bg_pid" 2> /dev/null; do
   current_time=$(date +%s)
   elapsed_time=$((current_time - start_time))
   
   if [ $elapsed_time -ge $timeout ]; then
-    >&2 echo "Postgres is unavailable - timeout after $timeout seconds"
+    >&2 echo "Timeout after $timeout seconds - exiting"
+    kill -9 "$bg_pid" 2> /dev/null
     exit 1
   fi
   
-  >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
 
->&2 echo "Postgres is up - executing command"
+# Execute the command if the background process completed successfully
 exec $cmd
